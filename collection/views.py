1d8dacfbdec3abc9e405f0696d99e81c4a1067ac
ronchris <ponferrada.ronald@gmail.com>
2017-03-06 19:18:20 -0800

    rendered Things views then index

--- collection/views.py
+++ collection/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from collection.models import Thing
 
 # Create your views here.
 def index(request):
 	# this is your new view
-	return render(request, 'index.html')
+	things = Thing.objects.all()
+	
+	return render(request, 'index.html', {
+		'things': things,
+	})
